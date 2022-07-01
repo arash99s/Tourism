5b61397c93b69789dd4874d3b4e1cca1f194cf15
MohamadReza <mohamadrezakamali2000@google.com>
2022-07-01 19:48:53 +0430

    add user panel page

---
+++ Views/User/panel.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/Tourism/views/styles/panel.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="col-lg-2 col-md-1 col-sm-1 col-xs-1"></div>
+    <div class="col-lg-8 col-md-10 col-sm-10 col-xs-10 container">
+        <hr>
+        <div class="col-lg-8 col-md-8 col-sm-7 col-xs-12 item">
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <label for="">user name :</label>
+            </div> 
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <p>mohamdreza</p>
+            </div>
+            <!-- <div class="col-lg-2"></div> -->
+            <hr>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <label for="">email : </label>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <p>mohamdreza@gmail.com</p>
+            </div>
+            <!-- <div class="col-lg-2"></div> -->
+            <hr>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" >
+                <label for="">firstname : </label>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <p>mohamdreza@gmail.com</p>
+            </div>
+            <!-- <div class="col-lg-2"></div> -->
+            <hr>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <label for="">lastname : </label>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <p>mohamdreza@gmail.com</p>
+            </div>
+            <hr>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <label for="">age : </label>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <p>mohamdreza@gmail.com</p>
+            </div>
+            <!-- <div class="col-lg-2"></div> -->
+            <hr>
+        </div>
+        
+        <div class="col-lg-4 col-md-4 col-sm-5 col-xs-12 item">
+            <div class="imgwrapper">
+                <img src="/Tourism/avatars/default.png" alt="">
+            </div>
+        </div>
+    
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 item2">
+            <div class="col-lg-4 col-md-8 col-xs-8">
+                <label for="">city : </label>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-4 col-xs-4">
+                <p>tehran</p>
+            </div>
+        </div>
+        
+        
+        <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 item2">
+            
+            <div class="col-lg-4 col-md-4 col-sm-8 col-xs-8">
+                <label for="">country : </label>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-4 col-xs-4">
+                <p>iran</p>
+            </div>
+        </div>
+        
+
+        <!-- <div class="col-lg-4"></div> -->
+        <hr>
+        <div class="col-lg-5"></div>
+        <div class="col-lg-2">
+            <input type="button" value="new journey">
+        </div>
+        <div class="col-lg-5"></div>
+    </div>
+    <div>
+       
+    </div>
+    <div class="col-lg-2 col-md-1 col-sm-1"></div>
+    
+    
+</body>
+</html>
