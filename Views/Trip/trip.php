468cab800260cd8f1cf307c6ecc1f2fa14f51fea
MohamadReza <mohamadrezakamali2000@google.com>
2022-07-03 02:17:49 +0430

    add trip page

---
+++ Views/Trip/trip.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/Tourism/views/styles/trip.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+
+    <title>Document</title>
+</head>
+<body>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container" style="display:flex ; justify-content:left;">
+            <a class="navbar-brand" href="#!" style="padding-top: 15px">Login</a>
+            <a class="navbar-brand" href="#!" style="padding-top: 15px">Panel</a>
+            <div style="justify-content:right ;align-self: flex-end;margin-left: auto;margin-right: 10px;" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container2 col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="slider-container col-lg-6 col-md-12 col-sm-12 col-xs-12">
+            <div class="menu">
+              <label for="slide-dot-1"></label>
+              <label for="slide-dot-2"></label>
+              <label for="slide-dot-3"></label>
+              <label for="slide-dot-4"></label>
+              <label for="slide-dot-5"></label>
+              <label for="slide-dot-6"></label>
+              <label for="slide-dot-7"></label>
+            </div>
+            
+            <input id="slide-dot-1" type="radio" name="slides" checked >
+              <div class="slide" style="background-image: url('../../avatars/default.png');"></div>
+            <input id="slide-dot-2" type="radio" name="slides">
+              <div class="slide" style="background-image: url('../../avatars/default.png');"></div>
+            <input id="slide-dot-3" type="radio" name="slides">
+              <div class="slide" style="background-image: url('../../avatars/default.png');"></div>
+            <input id="slide-dot-4" type="radio" name="slides">
+              <div class="slide" style="background-image: url('../../avatars/default.png');"></div>
+            <input id="slide-dot-5" type="radio" name="slides">
+              <div class="slide" style="background-image: url('../../avatars/default.png');"></div>
+            <input id="slide-dot-6" type="radio" name="slides">
+              <div class="slide" style="background-image: url('../../avatars/default.png');"></div>
+            <input id="slide-dot-7" type="radio" name="slides">
+              <div class="slide" style="background-image: url('../../avatars/default.png');"></div>
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
+          <i class='fas fa-ad fa-1x'></i>&nbsp;<label for="fadd"> آدرس کامل  : </label>
+          <p id="fadd">دگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع کاربردی می ب گذشته حال و آینده، شخلاقامید داشتاده قرار گیرد.
+          </p>
+          <i class='fas fa-comment-dollar'></i>&nbsp;<label for="costs">هزينه های سفر : </label>
+          <p id="costs">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم پگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائهیرد.
+          </p>
+          <i class='far fa-building'></i>&nbsp;<label for="his">تاریخچه بنا های دیده شده : </label>
+          <p id="his">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعا برای طراحان رایانرد، در ا گیرد.
+          </p>
+      
+        </div>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+          <i class='fas fa-place-of-worship'></i>&nbsp;<label for="suggest">مکان های پیشنهادی : </label>
+          <p id="suggest">لورم ایپسوم متن ساختگی با تولید سادگفاده قرار گیرد.
+          </p>
+          <i class="material-icons">&#xe7fb;</i>&nbsp;<label for="cul">فرهنگ مردم : </label>
+          
+            <p id="cul">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+            </p>
+            <i class="material-icons">&#xe535;</i>&nbsp;<label for="trans">نحوه جابه جایی در شهر : </label>
+  
+  
+            <p id="trans">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+            </p>
+            <i class="material-icons">&#xe32a;</i>&nbsp;<label for="sec">نکات امنیتی : </label>
+  
+  
+            <p id="sec">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+            </p>
+  
+            <i class='fas fa-comment'></i>&nbsp;<label for="others">توضیحات دیگر : </label>
+  
+  
+            <p id="others">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+            </p>
+          
+        </div>
+    </div>
+    
+    
+      
+</body>
+
+</html>
